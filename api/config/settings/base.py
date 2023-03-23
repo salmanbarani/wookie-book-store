7b9a9737e03fe11dd72be7cbcc6b21b5c0118a78
salmanbarani <salman.barani@postpay.io>
2023-03-24 01:15:17 +0300

    add celer, redis & flower

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -30,6 +30,7 @@ THIRD_PARTY_APPS = [
     'drf_yasg',
     'corsheaders',
     'django_extensions',
+    'djcelery_email',
 ]
 
 LOCAL_APPS = [
@@ -150,6 +151,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_URLS_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "UTC"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+
 
 LOGGING = {
     "version": 1,
--- api/config/settings/local.py
+++ api/config/settings/local.py
@@ -11,3 +11,10 @@ SECRET_KEY = env(
 )
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "salmanandb@outlook.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Wookie"
